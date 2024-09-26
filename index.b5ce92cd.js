cbc5f066f4333098e448b6b388804bd98cda50f8
Viktoriia Dereza <viktoriiadereza7@gmail.com>
2024-09-26 21:06:26 +0300

    make build

---
+++ index.b5ce92cd.js
@@ -0,0 +1,2 @@
+var e=document.getElementById("largeImg");document.getElementById("thumbs").addEventListener("click",function(t){t.preventDefault();var n=t.target.closest("a");if(n){var r=n.href;e.src=r}});
+//# sourceMappingURL=index.b5ce92cd.js.map
